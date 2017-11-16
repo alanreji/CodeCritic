b45adfad880859c44373119afd13a3b7f5078700
Alan Reji <alanreji.pulickal@gmail.com>
2017-11-17 00:58:57 +0530

    Add files via upload

--- assets/php/tailtemplate.php
+++ assets/php/tailtemplate.php
@@ -1,3 +1,8 @@
+        <div class="footer">
+            <p>
+                <a target="blank" href="http://cs19.club/history.php">Cs19.Club</a> © 2017 <br> Powered By CSE B
+            </p>
+        </div>
         <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
